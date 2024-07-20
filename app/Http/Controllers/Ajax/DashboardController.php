063a1637da57dafbd86f8031f89c380b54bdabc4
ThanhXT2002 <141716249+ThanhXT2002@users.noreply.github.com>
2024-07-20 04:40:47 +0000

    commit paginationArgrument

--- app/Http/Controllers/Ajax/DashboardController.php
+++ app/Http/Controllers/Ajax/DashboardController.php
@@ -4,16 +4,27 @@ namespace App\Http\Controllers\Ajax;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Language;
+use Illuminate\Support\Str;
 
 
 class DashboardController extends Controller
 {
+    protected $language;
 
     public function __construct(
        
     ){
-        
+        $this->middleware(function($request, $next){
+            $locale = app()->getLocale(); // vn en cn
+            $language = Language::where('canonical', $locale)->first();
+            $this->language = $language->id;
+          
+            return $next($request);
+        });
     }
+
+
 
     public function changeStatus(Request $request){
         $post = $request->input();
@@ -38,7 +49,41 @@ class DashboardController extends Controller
 
     }
 
-    
+    public function getMenu(Request $request){
+        $model= $request->input('model');
+        $serviceInterfaceNamespace = '\App\Repositories\\' . ucfirst($model) . 'Service';
+        if (class_exists($serviceInterfaceNamespace)) {
+            $serviceInstance = app($serviceInterfaceNamespace);
+        }
+        // $object = $serviceInterfaceNamespace->pagination($request, $this->language);
+        $agruments = $this->paginationArgrument($model);
+    }
+
+    private function paginationArgrument(string $model = ''):array{
+        $model = Str::snake($model);
+        return [
+            'select' => ['id', 'name'],
+            'condition' => [
+                'where' => [
+                    ['tb2,language_id', '=', $this->language],
+                ]
+            ],
+            'perpage' =>10,
+            'paginationConfig' => [
+                'path'=>$model.'/index',
+                'groupBy' => ['id', 'name']
+                
+            ],
+            'orderBy' => [$model.'s.id', 'DESC'],
+            'relations' => [],
+            'join' => [
+                [$model.'_language as tb2', 'tb2,post_id', '=', $model.'s.id'],
+                ['post_catalogue_post as tb3', 'post.id', '=', ' tb3.pist_id'],
+            ],
+        ];
+    }
+
+
    
    
 }
